19de295121a37d3a10579f84ec7b0fd216755fd2
Dr Michael Ajayi (STADIO - Centurion) <michaela@stadio.ac.za>
2025-07-03 14:21:35 +0200

    DockerFile

--- obj/Debug/net8.0/boardroombooking1.AssemblyInfo.cs
+++ obj/Debug/net8.0/boardroombooking1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("boardroombooking1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e65d96eed218522d74c798d8ed30d2941967b310")]
 [assembly: System.Reflection.AssemblyProductAttribute("boardroombooking1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("boardroombooking1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
